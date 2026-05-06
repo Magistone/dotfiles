80d46d05254fa2e2b2193538204ca1626f9adbca
Magistone <56304476+Magistone@users.noreply.github.com>
2026-05-06 07:24:02 +0200

    update python lsp

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,7 +1,7 @@
 return {
 	'mason-org/mason-lspconfig.nvim',
 	opts = {
-		ensure_installed = { 'lua_ls', 'rust_analyzer', 'html', 'eslint', 'clangd', 'gopls', 'pyright', 'htmx' },
+		ensure_installed = { 'lua_ls', 'rust_analyzer', 'html', 'eslint', 'clangd', 'gopls', 'basedpyright', 'htmx' },
 	},
 	dependencies = {
 		'neovim/nvim-lspconfig',
